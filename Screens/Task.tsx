7154bea2b1255b4973c3c6dec5de47c3727311cd
Peter May <peter.may@winzana.com>
2022-09-28 11:26:09 +0200

    syntax fix

--- Screens/Task.tsx
+++ Screens/Task.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 export default function Task() {
-  return <View>
-  <Text>This is a Task page</Text>
-</View>
+  return (
+    <View>
+      <Text>This is a Task page</Text>
+    </View>
+  );
 }
